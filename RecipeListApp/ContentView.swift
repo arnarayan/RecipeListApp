dd3c52feb24cedf9d328c635f70369d9a0b2c414
Anand.Narayan <arnarayan@gmail.com>
2022-02-05 09:55:20 -0700

    Initial Commit

---
+++ RecipeListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeListApp
+//
+//  Created by Anand Narayan on 2022-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Anand Narayan on 2022-02-05.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
